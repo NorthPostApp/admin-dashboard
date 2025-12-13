377cb32dc71735bd2dd1e9b469d29b246f35f99d
akane9506 <akane19950611@gmail.com>
2025-12-13 15:16:18 -0800

    create address from (ui) completed

--- src/components/header/ThemeSelector.tsx
+++ src/components/header/ThemeSelector.tsx
@@ -1,6 +1,5 @@
 import { THEMES, type Theme } from "@/consts/app-config";
 import { Select, SelectItem, SelectTrigger, SelectContent } from "@/components/ui/select";
-import { Label } from "@/components/ui/label";
 import { useAppContext } from "@/hooks/useAppContext";
 import { Sun, Moon, Monitor } from "lucide-react";
 
@@ -19,7 +18,7 @@ export default function ThemeSelector() {
   const { theme, updateTheme } = useAppContext();
   return (
     <section className="header-selector">
-      <Label>Theme</Label>
+      <p>Theme</p>
       <Select defaultValue={theme} onValueChange={(value) => updateTheme(value as Theme)}>
         <SelectTrigger className="header-selector__trigger">
           {getIcon(theme)}
